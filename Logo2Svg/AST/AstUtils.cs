da53a553e35caabfb6b1c263a2ad1d326284b97b
Alberto Simões <ambs@cpan.org>
2023-12-09 16:28:22 +0000

    Update that a bool is

--- Logo2Svg/AST/AstUtils.cs
+++ Logo2Svg/AST/AstUtils.cs
@@ -2,7 +2,7 @@ namespace Logo2Svg.AST;
 
 public static class AstUtils
 {
-    public static bool AsBool(this float v) => v is >= 1 or <= -1;
+    public static bool AsBool(this float v) => v is >= 0.5f or <= -0.5f;
     
     public static float AsFloat(this bool b) => b ? 1f : 0f;
 
